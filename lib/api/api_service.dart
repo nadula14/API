31af3bf6975cc084ffe13d06e2cf8fe3bac8db29
nadula14 <madugalle_ewmwwndb_e22@engug.ruh.ac.lk>
2024-12-12 12:47:54 +0530

    1stt

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -3,14 +3,14 @@ import 'package:api_practise/models/product_model.dart';
 class ApiService {
   //Fetch all products from Api
 
-  Future<List<Product>fetchAllProducts()async{
-    const String url = "https://fakestoreapi.com/products";
+  // Future<List<Product>fetchAllProducts()async{
+  //   const String url = "https://fakestoreapi.com/products";
 
-    try {
+  //   try {
 
-    }
-    catch(error){
-      print ("Error in fetching products: $error");
-    }
-  }
+  //   }
+  //   catch(error){
+  //     print ("Error in fetching products: $error");
+  //   }
+  // }
 }
