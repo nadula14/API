b3621a1c50a5ddc2346f5a5b73e39843e01d78ab
nadula14 <madugalle_ewmwwndb_e22@engug.ruh.ac.lk>
2024-12-12 12:47:22 +0530

    1st

---
+++ lib/api/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:api_practise/models/product_model.dart';
+
+class ApiService {
+  //Fetch all products from Api
+
+  Future<List<Product>fetchAllProducts()async{
+    const String url = "https://fakestoreapi.com/products";
+
+    try {
+
+    }
+    catch(error){
+      print ("Error in fetching products: $error");
+    }
+  }
+}
---
+++ lib/models/product_model.dart
@@ -0,0 +1,41 @@
+class Product {
+  final int? id;
+  final String title;
+  final String price;
+  final String description;
+  final String image;
+  final String category;
+
+  Product(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.image,
+      required this.category});
+
+   // Api eken json wdiht ena object ek dart object ekk karagnnwa
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'],
+      description: json['description'],
+      image: json['image'],
+      category: json['category'],
+    );
+  }
+   // dart eka json krna hati
+
+   Map <String, dynamic> toJson() {
+     return {
+       'id': id,
+       'title': title,
+       'price': price,
+       'description': description,
+       'image': image,
+       'category': category,
+     };
+   }
+
+}
